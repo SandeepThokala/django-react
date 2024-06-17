7d31f28ccc0eb4f4e6196b02c7c40ef9fefb0e78
SandeepThokala <imdeepu98@gmail.com>
2024-06-17 09:47:49 -0400

    Created Routes in React

--- musicController/ui/src/components/App.js
+++ musicController/ui/src/components/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
+import HomePage from './HomePage';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+// const App = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.test}</h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
-
-export default App
 
 const appDiv = document.getElementById("app");
 const root = createRoot(appDiv);
-root.render(<App />);
+root.render(<App test='test'/>);
--- musicController/ui/webpack.config.js
+++ musicController/ui/webpack.config.js
@@ -20,13 +20,13 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("production"),
-      },
-    }),
-  ],
+  }
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     "process.env": {
+  //       // This has effect on the react lib size
+  //       NODE_ENV: JSON.stringify("production"),
+  //     },
+  //   }),
+  // ],
 };
